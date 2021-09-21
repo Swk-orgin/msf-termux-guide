05e432ce2201a4ad76b205531b7903387a215f90
Learn-Termux <91124025+Learn-Termux@users.noreply.github.com>
2021-09-21 17:16:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,18 @@ pkg install php
 chmod +x start-msf
 
 rm $PREFIX/bin/start-msf
-
+rm -rf $HOME/msf-termux-core/
 mv start-msf $PREFIX/bin/
 
 mv $HOME/msf-termux-guide/.msf-termux-core/ $HOME
 
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+cyan='\033[36m'
+magneta='\033[1;35m'
+reset='\033[0m'
+
+echo
+echo -e "$green SucessFully Instslled"
+echo -e "$magneta Type start-msf To Run ..."
